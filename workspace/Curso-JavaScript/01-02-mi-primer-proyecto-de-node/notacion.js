7d720bf863bd304657b521714790a97e39e7eb99
JosePeresini <joseignacioperesini@gmail.com>
2023-02-09 00:34:25 -0300

    notacion ;-.-[]-()-{} de la sesion numero 01-02

---
+++ workspace/Curso-JavaScript/01-02-mi-primer-proyecto-de-node/notacion.js
@@ -0,0 +1,26 @@
+//! Notación
+// ; --> delimitar el final de una linea
+const b = 4;
+b + 4;
+
+// . --> Se usa en los objectos para acceder a los atributos
+//todo movil.anchura
+
+// [] --> Listas, arrays
+const ar = [1, 2, 3, 4];
+console.log(ar);
+
+// () --> Functions
+function suma(a, b) {
+    return a + b;
+}
+
+// {} --> Llaves para objetos, funciones y estructuras de control.
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if (true) {
+    const constString = "Esto es una string";
+}
