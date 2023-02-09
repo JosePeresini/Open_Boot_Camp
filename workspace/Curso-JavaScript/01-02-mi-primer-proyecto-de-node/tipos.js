e147c9b0aa0ba0fddc62bce4fecb92f1b5612bf1
JosePeresini <joseignacioperesini@gmail.com>
2023-02-08 23:13:21 -0300

    tipos de datos primitivos de la sesion numero 2

---
+++ workspace/Curso-JavaScript/01-02-mi-primer-proyecto-de-node/tipos.js
@@ -0,0 +1,28 @@
+//! Tipos primitivos
+
+//todo Number
+4, 5, 7;
+
+//* String
+"Hola mundo";
+//* Character String
+'a';
+`comillas simples`;
+
+//todo Booleans
+true;
+false;
+
+//! Null y Undefined
+null;
+undefined;
+
+//! Null, Undefined, False y 0 ===> Todas son falsy
+console.log(null === undefined);
+
+if (null) {
+    console.log("Cumple");
+} else {
+    console.log("No Cumple");
+}
+
