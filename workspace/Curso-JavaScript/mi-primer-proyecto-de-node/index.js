bf252bab69f3912d809b47fdf16d43a35eeba683
JosePeresini <joseignacioperesini@gmail.com>
2023-02-08 22:32:08 -0300

    intro al curso de javascript con gorka villar en open bootcamp

---
+++ workspace/Curso-JavaScript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,14 @@
+//todo Este es el mensaje de bienvenida!
+
+console.log("Esta es la puerta de entrada al proyecto");
+
+//todo Esta la parte intermedia!
+//? En esta parte realizamos los chequeos necesesarios!
+//! Ultima linea de saludo!
+//* Esta comentado
+/*
+! Esto es otro tipo de comentario!!! 
+*/
+
+//todo Este es el mensaje de Salida!
+console.log("Adiós");
