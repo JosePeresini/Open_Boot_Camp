25bd637cc2b6efe47d320a1c20e7a1baea730718
JosePeresini <joseignacioperesini@gmail.com>
2023-02-25 07:04:53 -0300

    gestion de errores con winston de la sesion numero 9

---
+++ workspace/Curso-JavaScript/09-gestion-de-errores/index.js
@@ -0,0 +1,7 @@
+const logger = require('./logger');
+
+// logger.log("Saliendo por pantalla")
+logger.info("Mensaje informativo");
+logger.debug("Mensaje de debug");
+logger.warn("Mensaje de advertencia");
+logger.error("Mensaje error");
