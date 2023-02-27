82e0bae1313c2b143dd72cf553b8d8b881d2b2ea
JosePeresini <joseignacioperesini@gmail.com>
2023-02-27 08:10:12 -0300

    insercion a modulos de export a import de la sesion numero 10

---
+++ workspace/4) Curso-JavaScript/10-modulos/index.js
@@ -0,0 +1,19 @@
+// formas de importar/exportar módulos
+// 1. CommonJS- require
+// 2. Import ES6
+
+// const moduloMatematicas = require("./modulos/matematicas");
+// const factorial = moduloMatematicas.factorial;
+// const { factorial, suma } = moduloMatematicas;
+// const suma = moduloMatematicas.suma;
+// console.log(moduloMatematicas);
+
+const { factorial, suma } = require("./modulos/matematicas")
+
+const fact = factorial(5);
+console.log(fact);
+
+const sum = suma(12, 90);
+console.log(sum);
+
+// console.log(module);
